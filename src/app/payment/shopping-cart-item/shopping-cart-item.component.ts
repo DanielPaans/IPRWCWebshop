829cd223cfc6e74144defba61546e59b857184b1
DanielPaans <daniel100tablets@gmail.com>
2021-12-23 16:07:43 +0100

    commit before npm install

--- src/app/payment/shopping-cart-item/shopping-cart-item.component.ts
+++ src/app/payment/shopping-cart-item/shopping-cart-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Product} from "../../shared/models/Product";
 
 @Component({
   selector: 'app-shopping-cart-item',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingCartItemComponent implements OnInit {
 
+  // @Input item: Product
   constructor() { }
 
   ngOnInit(): void {
