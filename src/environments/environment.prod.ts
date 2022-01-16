4d34b05448a7d5d24480b15912c7fb873da04584
DanielPaans <daniel100tablets@gmail.com>
2022-01-16 17:39:13 +0100

    added HTTP_CONFIG to environment.prod.ts, so it can run in production.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,16 @@
+const API_PATH = "http://localhost:8080/api";
+const IMAGE_PATH = "http://localhost/save.php";
 export const environment = {
-  production: true
+  production: true,
+  HTTP_CONFIG: {
+    API_PATH: API_PATH,
+    ADMIN_PATH: API_PATH + "/admin",
+    AUTH_PATH: API_PATH + "/authenticate",
+    USER_PATH: API_PATH + "/user",
+    PRODUCT_PATH: API_PATH + "/product",
+    ORDER_PATH: API_PATH + "/order",
+    CATEGORY_PATH: API_PATH + "/category",
+    IMAGE_PATH: IMAGE_PATH
+  },
+  PATH: "./assets/images/"
 };
